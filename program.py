1dae08616686541bdbe7311dd14189e21135de15
Milind Dalvi <milind.dalvi14@gmail.com>
2017-08-22 22:24:13 +0530

    Program sketched

---
+++ program.py
@@ -0,0 +1,33 @@
+import random
+
+
+class Table(object):
+    pass
+
+
+class TableRules(object):
+    pass
+
+
+class Player(object):
+    pass
+
+
+class Deck(object):
+    pass
+
+
+class Card(object):
+    pass
+
+
+class Dealer(object):
+    pass
+
+
+class Shoe(object):
+    pass
+
+
+class Seats(object):
+    pass
